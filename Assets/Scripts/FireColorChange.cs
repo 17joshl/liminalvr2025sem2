1e8d81d88921cfc09ac445352722b8fa850864e1
MarkDino47728191 <mark.dino@students.mq.edu.au>
2025-09-23 13:32:09 +1000

    shrinking script & fire VFX adjusted

--- Assets/Scripts/FireColorChange.cs
+++ Assets/Scripts/FireColorChange.cs
@@ -1,101 +1,154 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class FireColorChange : MonoBehaviour
 {
+    [Header("Color Settings")]
     public Color lookAtColor = Color.red;
     private Color originalColor;
-    private ParticleSystem.MainModule mainModule;
 
+    public Transform fireVisual;
 
-    public float growRate = 0.1f;
-    public float scaleCount = 0.1f;
-
+    [Header("Scale Settings")]
     public Vector3 startingScale = new Vector3(0.1f, 0.1f, 0.1f);
-    public Vector3 targetScale = new Vector3(0.5f, 0.5f, 0.5f);
-    public Vector3 finalScale = new Vector3(1f, 1f, 1f);
+    public Vector3 midScale = new Vector3(0.5f, 0.5f, 0.5f);   // Halfway
+    public Vector3 finalScale = new Vector3(1f, 1f, 1f);       // Full
 
+    [Tooltip("How fast the fire grows when looked at")]
+    public float growSpeed = 2f;
 
-    public float duration = 2f; 
-    public float lossDuration = 30f;
+    [Tooltip("How fast the fire shrinks when looked away from")]
+    public float shrinkSpeed = 2f;
 
+    [Header("Particles")]
+    public ParticleSystem smallFire;  
+    public ParticleSystem largeFire;  
 
-    public float noiseTarget = 2f; 
-
-
-    public ParticleSystem smallFire;  // Phase 2
-
-
+    private bool isLookedAt = false;
+    private bool phase2Active = false;
+    private bool phase3Active = false;   
+    private ParticleSystem mainParticles;
 
     void Start()
     {
-        ParticleSystem ps = GetComponent<ParticleSystem>();
+        fireVisual.localScale = startingScale;
 
-        if (ps != null)
+        mainParticles = GetComponent<ParticleSystem>();
+        if (mainParticles != null)
         {
-            mainModule = ps.main;
-            originalColor = mainModule.startColor.color;
+            var main = mainParticles.main;
+            originalColor = main.startColor.color;
         }
 
+        if (smallFire != null)
+        {
+            smallFire.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            smallFire.gameObject.SetActive(false);
+        }
+
+        if (largeFire != null)
+        {
+            largeFire.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            largeFire.gameObject.SetActive(false);
+        }
     }
 
-    void Update() {
-        if(transform.localScale == targetScale) {
+    void Update()
+    {
+        Vector3 target = isLookedAt ? finalScale : startingScale;
+        float speed = isLookedAt ? growSpeed : shrinkSpeed;
 
-            smallFire.gameObject.SetActive(true);
 
-            var emission = smallFire.emission;
-            emission.enabled = true;
+        fireVisual.localScale = Vector3.MoveTowards(
+            fireVisual.localScale,
+            target,
+            speed * Time.deltaTime
+        );
 
-            smallFire.Play(true);
-        } else {
-            smallFire.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+ 
+        float currentSize = fireVisual.localScale.x; 
+
+        // ---- Small fire (midpoint) ----
+        if (!phase2Active && currentSize >= midScale.x)
+        {
+            ActivateSmallFire();
+            phase2Active = true;
+        }
+        else if (phase2Active && currentSize < midScale.x)
+        {
+            DeactivateSmallFire();
+            phase2Active = false;
         }
 
+        // ---- Large fire (near full scale) ----
+        if (!phase3Active && currentSize >= finalScale.x * 0.80f) 
+        {
+            ActivateLargeFire();
+            phase3Active = true;
+        }
+        else if (phase3Active && currentSize < finalScale.x * 0.80f)
+        {
+            DeactivateLargeFire();
+            phase3Active = false;
+        }
     }
 
     public void OnLookAt()
     {
-        //mainModule.startColor = lookAtColor;
-
-                StartCoroutine(growOverTime(targetScale, duration));
-
-            
-    }
-
-
-    IEnumerator growOverTime(Vector3 endScale, float time)
+        isLookedAt = true;
+        if (mainParticles != null)
         {
-            Vector3 startScale = transform.localScale;
-            float elapsed = 0;
-
-            while (elapsed < time)
-            {
-                transform.localScale = Vector3.Lerp(startScale, endScale, elapsed / time);
-                //scaleCount += growRate;
-
-                Debug.Log(scaleCount);
-
-
-                elapsed += Time.deltaTime;
-                yield return null;
-            }
-            transform.localScale = endScale; 
+            var main = mainParticles.main; 
+            main.startColor = lookAtColor;
         }
-
-
-
+    }
 
     public void OnLookAway()
     {
-        mainModule.startColor = originalColor;
-
-
-            StartCoroutine(growOverTime(startingScale, lossDuration));
-
+        isLookedAt = false;
+        if (mainParticles != null)
+        {
+            var main = mainParticles.main; 
+            main.startColor = originalColor;
+        }
     }
 
+    private void ActivateSmallFire()
+    {
+        if (smallFire != null)
+        {
+            smallFire.gameObject.SetActive(true);
+            var emission = smallFire.emission;
+            emission.enabled = true;
+            smallFire.Play(true);
+        }
+    }
 
+    private void DeactivateSmallFire()
+    {
+        if (smallFire != null)
+        {
+            smallFire.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            smallFire.gameObject.SetActive(false);
+        }
+    }
 
+    private void ActivateLargeFire()
+    {
+        if (largeFire != null)
+        {
+            largeFire.gameObject.SetActive(true);
+            var emission = largeFire.emission;
+            emission.enabled = true;
+            largeFire.Play(true);
+        }
+    }
+
+    private void DeactivateLargeFire()
+    {
+        if (largeFire != null)
+        {
+            largeFire.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            largeFire.gameObject.SetActive(false);
+        }
+    }
 }
